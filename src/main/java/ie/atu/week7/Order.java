fb3254f25b0166762e2d8e8f76a58a1a76394bc6
MARTAPRIETOSOLOVYOVA <G00431347@atu.ie>
2024-11-04 09:12:10 +0000

    created order class and made getters and setters

--- src/main/java/ie/atu/week7/Order.java
+++ src/main/java/ie/atu/week7/Order.java
@@ -4,5 +4,45 @@ public class Order {
     private String orderID;
     private String customerName;
     private String productName;
+    private int quantity;
 
+
+    public Order(){ //default constructor
+        this.orderID = null;
+        this.customerName = null;
+        this.productName = null;
+        this.quantity = 0;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
