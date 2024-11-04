8c0e5656d0a9917eeb82cdfeba9edbfab6ff278f
MARTAPRIETOSOLOVYOVA <G00431347@atu.ie>
2024-11-04 09:07:52 +0000

    test commit

--- src/main/java/ie/atu/week7/Order.java
+++ src/main/java/ie/atu/week7/Order.java
@@ -2,7 +2,7 @@ package ie.atu.week7;
 
 public class Order {
     private String orderID;
-    private String orderID;
-    private String orderID;
+    private String customerName;
+    private String productName;
 
 }
